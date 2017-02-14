0b49465bc8675f8088db33512f3d011fad61eec6
Michael Russell <mrussell@schubergphilis.com>
2017-02-14 10:41:42 +0100

    Don't use docker-compose for running tests anymore

--- test.sh
+++ test.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 docker build -t saas/check_prometheus .
-docker run --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  --net=host \
-  saas/check_prometheus \
-  bash -c '\
-    trap "docker-compose down --rmi all > /dev/null; docker-compose rm -f > /dev/null" EXIT; \
-    ruby test.rb'
+docker run --rm saas/check_prometheus rspec -c -f d
