f048dc32faa4fa316f02af275566c6b8e1905b2e
Otávio Fernandes <ofernandes@schubergphilis.com>
2017-02-26 14:44:46 +0100

    Class renaming.

--- bin/check_prometheus.rb
+++ bin/check_prometheus.rb
@@ -7,18 +7,18 @@ config_file = ARGV[0] || 'config.yml'
 abort("Can't find configuration file at '#{config_file}'") \
   unless File.exist?(config_file)
 
-run = nil
+runner = nil
 
 begin
-  run = Sensu::Plugins::Prometheus::Checks::Runner.new(
+  runner = Sensu::Plugins::Prometheus::Checks::Runner.new(
     YAML.load_file(config_file)
   )
-  run.run
+  runner.run
 rescue RuntimeError => e
   puts "ERROR: #{e}"
   exit(1)
 end
 
-puts "\n"
-puts run.output
-exit(run.status)
+puts("\n")
+puts(runner.output)
+exit(runner.status)
