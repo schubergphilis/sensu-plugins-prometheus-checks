1a7afe5408db4f5d1ff7ac8289dc0c52d224d29c
Michael Russell <mrussell@schubergphilis.com>
2017-04-06 11:19:15 +0200

    Pass through codecov token to docker container

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 docker build -t saas/check_prometheus .
-docker run --rm -e TRAVIS=$TRAVIS saas/check_prometheus rspec -c -f d
+docker run --rm -e CODECOV_TOKEN=$CODECOV_TOKEN -e TRAVIS=$TRAVIS saas/check_prometheus rspec -c -f d
 docker run --rm saas/check_prometheus rubocop
